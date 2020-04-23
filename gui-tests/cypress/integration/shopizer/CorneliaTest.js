83861d02ad2b975df46fdee00fa3861125639fbf
Cornelia Hagstrom <corneliahagstrom@hotmail.com>
2020-04-23 11:17:36 +0200

    Cornelia andra commit

--- gui-tests/cypress/integration/shopizer/CorneliaTest.js
+++ gui-tests/cypress/integration/shopizer/CorneliaTest.js
@@ -1,6 +1,19 @@
 describe('My First Test', () => {
     it('Göra ett köp utan att logga in', () => {
         cy.visit('http://localhost:8080/shop');
-        //cy.get();
+        //let url ='URLEN LOCALHOST'
+        cy.get('[productid = "7"]').click(); //Lägg produkt 7 i varukorg
+        cy.get('#miniCartSummary > a > font > strong').should('have.text', '(1)');
+        //cy.get('#miniCartSummary > a > font > strong').should('include.text', '1');
+        //cy.contains('Checkout').click({force:true});
+        cy.get('#miniCartDetails > li.checkout-bg > a').click({force:true});
+        //Assertion att det är rätt url
+        cy.url().should('include', '/shop/cart/shoppingCart.html')
+        
+        
+        
+        
+        
+        cy.get('selector').type('skriva');
     })
   })
