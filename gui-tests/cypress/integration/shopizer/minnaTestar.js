7022e897fb48db64e160ad5137fbea645fd9f8fb
Minna Pettersson <minnapn@hotmail.com>
2020-04-28 11:45:22 +0200

    Modifierat test för att fungera med ny version av SUT + lagt till Daniels senaste tester i gemensam testfil

--- gui-tests/cypress/integration/shopizer/minnaTestar.js
+++ gui-tests/cypress/integration/shopizer/minnaTestar.js
@@ -1,43 +1,58 @@
-beforeEach(() => {
-    adminLogin();
-    cy.visit('http://localhost:8080/shop');
-  })
 
-describe('GUI tests for Shopizer', () => {
-    it('makes purchase without creating account', () => { 
-        cy.get('#featuredItemsContainer > div:nth-child(1) > div.product-content.text-center > div > div > a').click(); //Ändra selektor
-        //lägg till assert att varukorg visar 1
-        cy.get('#miniCartDetails > li.checkout-bg > a').click({force:true});
-        //lägg till assert på rätt produkt, price och total
-        cy.contains('Proceed to checkout').click();
-        cy.get('[name = "customer.billing.firstName"]').type('Testan');
-        cy.get('[name = "customer.billing.lastName"]').type('Testsson');
-        cy.get('[name = "customer.billing.address"]').type('Testvägen 5');
-        cy.get('[name = "customer.billing.city"]').type('Teststaden');
-        cy.get('[name = "customer.billing.postalCode"]').type('12345');
-        cy.get('[name = "customer.emailAddress"]').type('testan@testsson.se');
-        cy.get('[name = "customer.billing.phone"]').type('0701234567');
-        cy.get('#submitOrder').click();
+
+
+
+describe("Test shopizer", function () {
+
+    this.beforeAll('open shopizer', function () {
+        adminLogin();
+        const data = cy.readFile('testDataShopizer.json');
+    });
+
+    this.beforeEach('open shopizer', function () {
+        const url = "http://localhost:8080/shop/"
+        cy.visit(url);
+    });
+
+
+
+
+        it('makes purchase without creating account', () => {
+            let firstName = data[1].firstName;
+            console.log(firstName);
+            cy.get('#featuredItemsContainer > div:nth-child(1) > div.product-content.text-center > div > div > a').click(); //Ändra selektor
+            //lägg till assert att varukorg visar 1
+            cy.get('#miniCartDetails > li.checkout-bg > a').click({ force: true });
+            //lägg till assert på rätt produkt, price och total
+            cy.contains('Proceed to checkout').click();
+            cy.get('[name = "customer.billing.firstName"]').type(firstName);
+            cy.get('[name = "customer.billing.lastName"]').type('Testsson');
+            cy.get('[name = "customer.billing.address"]').type('Testvägen 5');
+            cy.get('[name = "customer.billing.city"]').type('Teststaden');
+            cy.get('[name = "customer.billing.postalCode"]').type('12345');
+            cy.get('[name = "customer.emailAddress"]').type('testan@testsson.se');
+            cy.get('[name = "customer.billing.phone"]').type('0701234567');
+            cy.get('#submitOrder').click();
+        })
+
+        xit('creates account', () => {
+            cy.contains('My Account').click();
+            cy.get('#registerLink').click();
+            cy.get('[name = "billing.firstName"]').type('Testan');
+            cy.get('[name = "billing.lastName"]').type('Testsson');
+            cy.get('[name = "billing.stateProvince"]').type('Teststaden');
+            cy.get('[name = "emailAddress"]').type('testan@testsson.se');
+            cy.get('[name = "password"]').type('password1');
+            cy.get('#passwordAgain').type('password1');
+
+
+        })
+
     })
 
-   it('creates account', () => {
-        cy.contains('My Account').click();
-        cy.get('#registerLink').click();
-        cy.get('[name = "billing.firstName"]').type('Testan');
-        cy.get('[name = "billing.lastName"]').type('Testsson');
-        cy.get('[name = "billing.stateProvince"]').type('Teststaden');
-        cy.get('[name = "emailAddress"]').type('testan@testsson.se');
-        cy.get('[name = "password"]').type('password1');
-        cy.get('#passwordAgain').type('password1');
-
-
-   })
-
-  })
-
-  function adminLogin(){
-    cy.visit('http://localhost:8080/admin');
-    cy.get('#username').type('admin');
-    cy.get('#password').type('password');
-    cy.get('#formSubmitButton').click();
-  }
+    function adminLogin() {
+        cy.visit('http://localhost:8080/admin');
+        cy.get('#username').type('admin');
+        cy.get('#password').type('password');
+        cy.get('#formSubmitButton').click();
+    }
--- gui-tests/cypress/integration/shopizer/shopizerGUItests.js
+++ gui-tests/cypress/integration/shopizer/shopizerGUItests.js
@@ -1,10 +1,7 @@
 
 
 describe("Test shopizer", function () {
-
-    this.beforeAll('open shopizer', function () {
-        adminLogin();
-    });
+    
 
     this.beforeEach('open shopizer', function () {
         const url = "http://localhost:8080/shop/"
@@ -16,18 +13,18 @@ describe("Test shopizer", function () {
 
     it('Göra ett köp utan att logga in', () => {
 
-
         //Lägg produkt 7 i varukorg
         cy.get('[productid = "7"]').click();
-        //assert att varukorgen fick en etta för hur många varor
-        cy.get('#miniCartSummary > a > font > strong').should('have.text', '(1)');
 
-        cy.get('#miniCartDetails > li.checkout-bg > a').click({ force: true });
+        //assert att varukorgen fick en etta för hur många varor    
+        cy.get('#miniCartSummary').should($el => expect($el.text().trim()).to.equal('1'));
+    
+        cy.contains('Checkout').click({ force: true });
 
         //Assertion att det är rätt url - att man kommer in i shoppingcart
         cy.url().should('include', '/shop/cart/shoppingCart.html')
 
-        cy.get('body > div.entry-header-area.ptb-40 > div > div > div > div > h1').should('include.text', 'Review your order');
+        cy.get('h1').should('include.text', 'Review your order');
 
         cy.contains('Proceed to checkout').click();
         cy.get('[name = "customer.billing.firstName"]').type('Testan');
@@ -46,28 +43,86 @@ describe("Test shopizer", function () {
     })
 
 
+
+    it("Test shoppingcart", function() {
+        cy.get('a[productid="4"]').click();
+
+        // one item in cart
+        cy.get('#miniCartSummary').should($el => expect($el.text().trim()).to.equal('1'));
+
+        // two items in cart
+        cy.get('a[productid="8"]').click();
+        cy.get('#miniCartSummary').should($el => expect($el.text().trim()).to.equal('2'));
+
+        // three items in cart
+        cy.get('a[productid="8"]').click();
+        cy.get('#miniCartSummary').should($el => expect($el.text().trim()).to.equal('3'));
+
+        // remove onei
+        cy.get('.close-icon > button[productid="4"]').click({force:true});
+        cy.get('#miniCartSummary').should($el => expect($el.text().trim()).to.equal('2'));
+
+        // remove last two
+        cy.get('.close-icon > button[productid="8"]').click({force:true});
+        cy.get('#miniCartSummary').should($el => expect($el.text().trim()).to.equal('0'));
+
+    });
+
     it('creates account', () => {
         cy.contains('My Account').click();
         cy.get('#registerLink').click();
-        cy.get('[name = "billing.firstName"]').type('Testan');
+        let firstName = 'Testan'
+        cy.get('[name = "billing.firstName"]').type(firstName);
         cy.get('[name = "billing.lastName"]').type('Testsson');
         cy.get('[name = "billing.stateProvince"]').type('Teststaden');
-        let date = new Date();
-        cy.get('[name = "emailAddress"]').type('testan' + date.getTime() + '@testsson.se');
+        const time = (new Date()).getTime();
+        cy.get('[name = "emailAddress"]').type('testan' + time + '@testsson.se');
         cy.get('[name = "password"]').type('password1');
         cy.get('#passwordAgain').type('password1');
         cy.contains('Create an account').click();
         cy.url().should('include', '/shop/customer/dashboard.html');
 
+        cy.get('#customerAccount > a > span > span').should('have.text', firstName);
+
 
     })
+
+    it('Can\'t create account when already exists', () => {
+
+        // go to registration
+        cy.contains('My Account').click();
+        cy.get('#registerLink').click();
+
+        // enter data in form
+        cy.get('[name = "billing.firstName"]').type('John');
+        cy.get('[name = "billing.lastName"]').type('Doe');
+        cy.get('#registration_country').select('Canada');
+        cy.get('[name = "billing.zone"]').select('Ontario');
+        cy.get('[name = "emailAddress"]').type('test@gmail.com'); 
+        cy.get('[name = "password"]').type('password1');
+        cy.get('#passwordAgain').type('password1');
+
+        // click submit button
+        cy.contains('Create an account').click();
+
+        // assert error message is displayed
+        cy.get('div.alert').should('contain', 'already exists');
+
+   })
+
+   it('Admin login', () => {
+    cy.visit('http://localhost:8080/admin');
+    cy.get('#username').type('admin@shopizer.com');
+    cy.get('#password').type('password');
+    cy.get('#formSubmitButton').click();
+
+    //Assert sidan innehåller Recent orders
+    cy.get('body').should('contain', 'Recent orders');
+    
+
+   })
 })
 
 
-
-function adminLogin() {
-    cy.visit('http://localhost:8080/admin');
-    cy.get('#username').type('admin');
-    cy.get('#password').type('password');
-    cy.get('#formSubmitButton').click();
+    
 }
